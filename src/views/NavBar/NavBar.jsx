97079951c9215fe528d9cdd679d38df47bb53acb
AEscobarAngola <abrancho1908@gmail.com>
2024-10-07 19:19:35 +0200

    feat:nav bar css styles

--- src/views/NavBar/NavBar.jsx
+++ src/views/NavBar/NavBar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { CNavigation } from '../../components/CNavigation/CNavigation'
+import './NavBar.css'
 
 export const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <>
+            <div className='nav-main-content'>
+                <div className='nav-logo'>
+                    logo
+                </div>
+                <div className='nav-elements'>
+                    <div ><CNavigation path='/' content={<img className='nav-icon-small' src="../images/home.svg" alt="home-icon-nav"/>} /></div>
+                    <div ><CNavigation path='/' content={<img className='nav-icon-small' src="../images/reservation.svg" alt="home-icon-nav"/>} /></div>
+                    <div ><CNavigation path='/' content={<img className='nav-icon-small' src="../images/historie.svg" alt="home-icon-nav"/>} /></div>
+                </div>
+                <div className='nav-exit'>
+                <div ><CNavigation path='/' content={<img className='nav-icon-middle' src="../images/exit.svg" alt="home-icon-nav"/>} /></div>
+                </div>
+            </div>
+        </>
+    )
 }
