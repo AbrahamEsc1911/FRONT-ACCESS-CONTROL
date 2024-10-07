5b5a08592d3d2783eb8084fd66dc1c305582507c
AEscobarAngola <abrancho1908@gmail.com>
2024-10-07 22:25:54 +0200

    feat: login view created

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Register } from '../Register/Register'
+import Login from '../Login/Login'
 
 export const Body = () => {
   return (
     <>
     <Routes>
       <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
     </>
   )
