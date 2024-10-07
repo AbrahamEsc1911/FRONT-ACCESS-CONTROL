97fcd15504f19d15a13f33b40c638c75b2ce2284
AEscobarAngola <abrancho1908@gmail.com>
2024-10-07 22:18:40 +0200

    feat: api call services for register new users it have been created

---
+++ src/Services/auth.services.js
@@ -0,0 +1,18 @@
+const URL = "http://localhost:3040/api/auth"
+
+export const register = async (credentials) => {
+    try {
+        const response = await fetch(`${URL}/register`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        return await response.json()
+
+    } catch (error) {
+        console.error(`Error fetching data ${error}`)
+    }
+}
