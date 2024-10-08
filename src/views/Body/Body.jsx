bb3871d563cb2c76f13383bbadacd70e41b04dc3
AEscobarAngola <abrancho1908@gmail.com>
2024-10-08 22:15:02 +0200

    feat: home view created, and added route to body

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Register } from '../Register/Register'
 import Login from '../Login/Login'
+import { Home } from '../Home/Home'
 
 export const Body = () => {
   return (
@@ -9,6 +10,7 @@ export const Body = () => {
     <Routes>
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/home' element={<Home />} />
     </Routes>
     </>
   )
