13026867c07afc396d6a4a51db1e76f851edccb2
AEscobarAngola <abrancho1908@gmail.com>
2024-10-08 21:18:33 +0200

    feat: api call login services created

--- src/Services/auth.services.js
+++ src/Services/auth.services.js
@@ -16,3 +16,20 @@ export const register = async (credentials) => {
         console.error(`Error fetching data ${error}`)
     }
 }
+
+export const login = async (credentials) => {
+    try {
+        const response = await fetch(`${URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        return await response.json()
+
+    } catch (error) {
+        console.error(`Error fetching data ${error}`)
+    }
+}
