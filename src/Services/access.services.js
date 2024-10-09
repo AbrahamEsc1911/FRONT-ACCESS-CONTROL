b0920b866697cb133430ff9e66e3ecc295e426d6
AEscobarAngola <abrancho1908@gmail.com>
2024-10-09 18:41:06 +0200

    feat: exit api call created

--- src/Services/access.services.js
+++ src/Services/access.services.js
@@ -1,9 +1,26 @@
 const URL = 'http://localhost:3040/api/access/'
 
-export const Access = async (roomId, token) => {
+export const access = async (roomId, token) => {
     try {
         const response = await fetch(`${URL}/${roomId}`, {
             method: 'POST',
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+
+        return await response.json()
+
+    } catch (error) {
+        console.error(`Error fetching data ${error}`)
+    }
+}
+
+export const exit = async (token) => {
+    try {
+        const response = await fetch(`${URL}/`, {
+            method: 'PUT',
             headers: {
                 "Content-type": "application/json",
                 "Authorization": `Bearer ${token}`
