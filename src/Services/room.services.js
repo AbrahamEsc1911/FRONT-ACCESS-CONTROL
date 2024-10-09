74245954552fc8d38955ed75e47a7ee3488fa1b0
AEscobarAngola <abrancho1908@gmail.com>
2024-10-09 16:09:07 +0200

    feat: get all rooms api call created, and migration to access histories

--- src/Services/room.services.js
+++ src/Services/room.services.js
@@ -1,18 +1,17 @@
-const URL = 'http://localhost:3040/api/access/'
+const URL = 'http://localhost:3040/api/rooms'
 
-export const Access = async (roomId, token) => {
+export const getAllRooms = async () => {
     try {
-        const response = await fetch(`${URL}/${roomId}`, {
-            method: 'POST',
+        const response = await fetch(`${URL}`, {
+            method: 'GET',
             headers: {
-                "Content-type": "application/json",
-                "Authorization": `Bearer ${token}`
+                "Content-type": "application/json"
             }
         })
 
         return await response.json()
-        
+
     } catch (error) {
         console.error(`Error fetching data ${error}`)
     }
-} 
+}
