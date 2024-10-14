86080a1c356be294abb99383bd0b33a12ace911d
AEscobarAngola <abrancho1908@gmail.com>
2024-10-14 22:40:35 +0200

    feat: bringing future reservations of user and mapping all the array of responses

--- src/views/MyReservations/MyReservations.jsx
+++ src/views/MyReservations/MyReservations.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getFutureReservationsByUserId } from '../../Services/access.services'
 
 export const MyReservations = () => {
+
+  const passport = JSON.parse(localStorage.getItem("passport"))
+  let token = null
+  if (passport) {
+    token = passport.token
+  }
+
+  const navigate = useNavigate()
+  const [futureReservations, setFutureReservations] = useState([])
+  const [serverMessage, setserverMessage] = useState("")
+  const [showServerMessage, setshowServerMessage] = useState(false)
+
+  useEffect(() => {
+    if (passport) {
+      const bringUserData = async () => {
+        const bringReservations = await getFutureReservationsByUserId(token)
+        console.log(bringReservations)
+        if (bringReservations.success) {
+          setFutureReservations(bringReservations.data)
+          setshowServerMessage(false)
+        } else {
+          setserverMessage(bringReservations.message)
+          setshowServerMessage(true)
+        }
+      };
+      bringUserData()
+
+    } else {
+      navigate('./login')
+    }
+
+  }, [])
+
+
   return (
-    <div>MyReservations</div>
+    <>
+      <div>
+        {futureReservations.length > 0 && futureReservations.map((element) => {
+          const entryDate = new Date(element.entry_date);
+          const exitDate = new Date(element.exit_date);
+          return <div key={element.id}>
+            <div>{element.room.room}</div>
+            <div>{element.state}</div>
+            <div>Fecha de entrada: {entryDate.toLocaleDateString()}</div>
+            <div>Hora de entrada: {entryDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
+            <div>Fecha de salida: {exitDate.toLocaleDateString()}</div>
+            <div>Hora de salida: {exitDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
+          </div>
+        })}
+      </div>
+
+    </>
   )
 }
