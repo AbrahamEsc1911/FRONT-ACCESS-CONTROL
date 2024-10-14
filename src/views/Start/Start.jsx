8634a96b23df2e075857e31f33171681ae42d2b4
AEscobarAngola <abrancho1908@gmail.com>
2024-10-14 23:21:46 +0200

    feat: adding buttons and use effect to check if there are or not passport

--- src/views/Start/Start.jsx
+++ src/views/Start/Start.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { CInputs } from '../../components/CInputs/CInputs'
+import { useNavigate } from 'react-router-dom'
 
 export const Start = () => {
+
+  const passport = JSON.parse(localStorage.getItem('passport'))
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(passport) {
+      navigate('/')
+    }
+  }, [])
+  
+  const btnRegister = () => {
+    navigate('/register')
+  }
+
+  const btnLogin = () => {
+    navigate('/login')
+  }
+
   return (
-    <div>Start</div>
+    <div>
+      
+      Access Control app for the Naves
+
+      <div>
+      <CInputs type='button' name='Register' value='Register' onClick={btnRegister} />
+      </div>
+      <div>
+      <CInputs type='button' name='Login' value='Login' onClick={btnLogin} />
+      </div>
+
+    </div>
   )
 }
