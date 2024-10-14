1b93c7cebb195fbd5101d0eec94c9b9993467678
AEscobarAngola <abrancho1908@gmail.com>
2024-10-14 19:23:23 +0200

    feat: my reservations view created

--- src/views/Body/Body.jsx
+++ src/views/Body/Body.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import { Register } from '../Register/Register'
 import Login from '../Login/Login'
 import { Home } from '../Home/Home'
+import { Reservations } from '../Reservations/Reservations'
+import { MyReservations } from '../MyReservations/MyReservations'
 
 export const Body = () => {
   return (
@@ -12,8 +14,8 @@ export const Body = () => {
       <Route path='/' element={<Home />} />
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/reservations'/>
-      <Route path='/my-reservations'/>
+      <Route path='/reservations' element={<Reservations />}/>
+      <Route path='/my-reservations' element={<MyReservations />} />
       <Route path='/start'/>
       <Route path='/profile'/>
     </Routes>
