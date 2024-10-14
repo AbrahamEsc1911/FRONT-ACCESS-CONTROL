8fdf7b1286418cfef855af84cb00b1f232759f37
AEscobarAngola <abrancho1908@gmail.com>
2024-10-14 23:51:32 +0200

    feat: getting user profile on profile view

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { userProfile } from '../../Services/user.services'
 
 export const Profile = () => {
+
+  const passport = JSON.parse(localStorage.getItem('passport'))
+  let token = null
+  if (passport) {
+    token = passport.token
+  }
+
+  const navigate = useNavigate()
+  const [userDataProfile, setUserDataProfile] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    dni: "",
+    StartUp: ""
+  })
+
+  useEffect(() => {
+    if (passport) {
+      const bringUserData = async () => {
+        const bringUserProfile = await userProfile(token)
+        if (bringUserProfile.success) {
+          setUserDataProfile(bringUserProfile.data)
+        } else {
+          localStorage.removeItem("passport");
+          navigate('/login')
+        }
+      };
+
+      bringUserData()
+
+    } else {
+      navigate('/login')
+    }
+  }, [])
+
   return (
-    <div>Profile</div>
+    <>
+      <div>name: {userDataProfile.name}</div>
+      <div>dni: {userDataProfile.dni}</div>
+      <div>email: {userDataProfile.email}</div>
+      <div>phone: {userDataProfile.phone}</div>
+      <div>StartUp: {userDataProfile.StartUp}</div>
+    </>
   )
 }
