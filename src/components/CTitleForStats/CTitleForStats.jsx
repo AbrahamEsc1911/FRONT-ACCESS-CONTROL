ad8d050b142b697bd890c93ffaf940737cf1fd41
AEscobarAngola <abrancho1908@gmail.com>
2024-10-15 21:23:48 +0200

    update: correction on css for sizes and margins and other elements

--- src/components/CTitleForStats/CTitleForStats.jsx
+++ src/components/CTitleForStats/CTitleForStats.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import './CtitleForStats.css'
 
-export const CTitleForStats = ({title1, title2, title3, title4, title5}) => {
-  return (
-    <>
-    <div className='table-row'>
-            <div className='table-row-content'>
-              <div className='row-text'>
-                <h4>
-                {title1}
-                </h4></div>
-                <div className='row-text'>
-                <h4>
-                {title2}
-                </h4></div><div className='row-text'>
-                <h4>
-                {title3}
-                </h4></div><div className='row-text'>
-                <h4>
-                {title4}
-                </h4></div><div className='row-text'>
-                <h4>
-                {title5}
-                </h4></div>
+export const CTitleForStats = ({ title1, title2, title3, title4, title5 }) => {
+    return (
+        <>
+            <div className='table-row'>
+                <div className='table-row-content'>
+                    <div className='row-text-long'>
+                        <h4>
+                            {title1}
+                        </h4></div>
+                    <div className='row-text'>
+                        <h4>
+                            {title2}
+                        </h4></div>
+                    <div className='row-text'>
+                        <h4>
+                            {title3}
+                        </h4></div>
+                    <div className='row-text'>
+                        <h4>
+                            {title4}
+                        </h4></div>
+                    <div className='row-text'>
+                        <h4>
+                            {title5}
+                        </h4></div>
+                </div>
+                <div className='table-division'></div>
             </div>
-            <div className='table-division'></div>
-          </div>
-    </>
-  )
+        </>
+    )
 }
