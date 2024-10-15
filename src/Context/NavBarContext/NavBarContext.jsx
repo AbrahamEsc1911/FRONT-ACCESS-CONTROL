d4f831f2c6187cbb43872674228a4b3c5938a26f
AEscobarAngola <abrancho1908@gmail.com>
2024-10-15 19:28:34 +0200

    feat: new context created, this content allows to change boolean for state of nav bar

---
+++ src/Context/NavBarContext/NavBarContext.jsx
@@ -0,0 +1,12 @@
+import React, { createContext, useState } from 'react'
+
+export const NavBarContext = createContext()
+
+export const NavBarProvider = ({ children }) => {
+
+    const [navBar, setNavBar] = useState(false)
+
+    return <NavBarContext.Provider value={{ navBar, setNavBar }}>
+        {children}
+    </NavBarContext.Provider>
+}
