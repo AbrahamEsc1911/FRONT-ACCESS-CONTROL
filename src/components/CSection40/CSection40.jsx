effa49305e1fff3db9f5a8820b5ea7837cd6303d
AEscobarAngola <abrancho1908@gmail.com>
2024-10-16 20:51:28 +0200

    feat: new component for make a section on body of 40 per cent size

---
+++ src/components/CSection40/CSection40.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSection40.css'
+
+export const CSection40 = ({ content }) => {
+    return (
+        <>
+            <div className="section-40">
+                {content}
+            </div>
+        </>
+    )
+}
