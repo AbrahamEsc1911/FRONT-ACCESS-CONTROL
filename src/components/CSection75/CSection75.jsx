e4211e9e2b1b6caf85154efa9fd059f4a976e482
AEscobarAngola <abrancho1908@gmail.com>
2024-10-16 22:57:24 +0200

    feat: new custom component for 75 per cent of body view

---
+++ src/components/CSection75/CSection75.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CSection75.css'
+
+export const CSection75 = ({content}) => {
+    return (
+        <>
+            <div className="section-75">
+                {content}
+            </div>
+        </>
+
+    )
+}
