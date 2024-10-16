0bbe07ea35c721811f6626e263008499436981a4
AEscobarAngola <abrancho1908@gmail.com>
2024-10-16 22:56:33 +0200

    feat: creating a new component for 25 per cent size of view

---
+++ src/components/CSection25/CSection25.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSection25.css'
+
+export const CSection25 = ({content}) => {
+    return (
+        <>
+            <div className="section-25">
+                {content}
+            </div>
+        </>
+    )
+}
