98354f5c2b5b7548912e5c718001e6673db2c461
AEscobarAngola <abrancho1908@gmail.com>
2024-10-17 20:17:43 +0200

    creating a new component of image and button

---
+++ src/components/CImageBlock/CImageBlock.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './CImageBlock.css'
+
+export const CImageBlock = () => {
+  return (
+    <>
+    <div className='about-building'>
+              <div>
+                <h2>Smart Building</h2>
+              </div>
+              <div id='btn-box'>
+                <CInputs type='button' value='Know more' className='btn-about-more' />
+              </div>
+            </div>
+    </>
+  )
+}
